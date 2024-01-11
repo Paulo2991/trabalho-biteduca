adc5667f91b08ff2671761e1998229c7a6ff8b4a
Paulo2991 <paulovaz38034@gmail.com>
2024-01-10 22:13:56 -0300

    Trabalho De Filmes Da Biteduca

---
+++ js/script.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const camposForm = document.querySelectorAll("[required]");
+
+    camposForm.forEach((campo) => {
+        campo.addEventListener("blur", () => {
+            validarCampo(campo);
+        });
+    });
+
+    function validarCampo(campo) {
+        const valorCampo = campo.value;
+        const erroElement = document.getElementById(`erro${campo.id.charAt(0).toUpperCase() + campo.id.slice(1)}`);
+
+        if (valorCampo.trim() === '') {
+            erroElement.innerHTML = `<p>O campo não pode estar vazio.</p>`;
+        }else if (campo.id === "contato" && valorCampo.length < 3 ) {
+            erroElement.innerHTML = `<p>O campo contato precisa ser preenchido com mais de 3 digitos.</p>`;
+        }else if (campo.id === "email" && !validarEmail(valorCampo)) {
+           erroElement.innerHTML = `<p>O email inserido é inválido.</p>`;
+        }else if(campo.id === "cpf" && !validarCPF(valorCampo)){
+            erroElement.innerHTML = `<p>O CPF inserido é inválido.</p>`;
+        }else {
+            erroElement.innerHTML = ``;
+    }    
+}
+
+function validarCPF(cpf) {
+    cpf = cpf.replace(/[^\d]+/g, "");
+    if (cpf == "") return false;
+  // Elimina CPFs invalidos conhecidos
+    if (
+        cpf.length != 11 ||
+        cpf == "00000000000" ||
+        cpf == "11111111111" ||
+        cpf == "22222222222" ||
+        cpf == "33333333333" ||
+        cpf == "44444444444" ||
+        cpf == "55555555555" ||
+        cpf == "66666666666" ||
+        cpf == "77777777777" ||
+        cpf == "88888888888" ||
+        cpf == "99999999999"
+        )
+        return false;
+  // Valida 1o digito
+    add = 0;
+    for (i = 0; i < 9; i++) add += parseInt(cpf.charAt(i)) * (10 - i);
+        rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11) rev = 0;
+    if (rev != parseInt(cpf.charAt(9))) return false;
+  // Valida 2o digito
+    add = 0;
+    for (i = 0; i < 10; i++) add += parseInt(cpf.charAt(i)) * (11 - i);
+        rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11) rev = 0;
+    if (rev != parseInt(cpf.charAt(10))) return false;
+    return true;
+}
+
+
+function validarEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+});
